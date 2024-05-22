5eaf818478891684b8802af84764add4ed5c6fbe
mike <sammartino.mike@gmail.com>
2024-05-22 15:31:41 -0400

    create wordRoutes.js and configure routerHandler to use word routes with '/words'

---
+++ server/routes/wordRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { Dog } = require('../db/index');
+
+// **************** GET ROUTES ********************
+
+// **************** POST ROUTES ********************
+
+// **************** PUT ROUTES ********************
+
+// **************** DELETE ROUTES ********************
+
+
+module.exports = router;
