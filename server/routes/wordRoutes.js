a0fc0caef8a2fc2a8fd6b0059a6babdbcd9aa916
mike <sammartino.mike@gmail.com>
2024-05-22 16:28:31 -0400

    commit before upstream update

--- server/routes/wordRoutes.js
+++ server/routes/wordRoutes.js
@@ -4,11 +4,50 @@ const { Dog } = require('../db/index');
 
 // **************** GET ROUTES ********************
 
+// GET WORDS BY DOG ID
+
+router.get('/', (req, res) => {
+  
+  // this route should get the all words from a specific dog
+  // the data sent in response will be used to populate the dogtionary
+  //
+
+
+  res.sendStatus(200);
+})
+
 // **************** POST ROUTES ********************
 
-// **************** PUT ROUTES ********************
+// POST WORD BY DOG ID
+
+router.post('/', (req, res) => {
+
+  // this route should handle adding a word to a specific dog's
+  //    word key
+
+})
+
+// **************** PUT/PATCH ROUTES ********************
+
+// UPDATE WORD BY DOG ID
+
+router.put('/', (req, res) => {
+
+  // this route should handle flipping the used boolean to true
+  // also handle marking word as a favorite
+
+})
 
 // **************** DELETE ROUTES ********************
+
+// DELETE WORD BY DOG ID
+
+router.delete('/', (req, res) => {
+
+  // this route should handle deleting a word from a specific
+  //    dog's dogtionary
+
+})
 
 
 module.exports = router;
