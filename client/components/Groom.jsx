dc09613fde10a0437f2e56700fc0ee9f4a32f9f4
cody-daigle <cody.h.daigle@gmail.com>
2024-05-23 14:11:06 -0500

    Feat / Created Groom component for a shop

--- client/components/Groom.jsx
+++ client/components/Groom.jsx
@@ -1,26 +1,57 @@
+/************** Cody D. Legacy **********/
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Col, Row, Container } from 'react-bootstrap/';
-import Dog from './Dog.jsx';
 
-function Groom({ dogs }) {
+function Groom() {
   const [user, setUser] = useState({});
   const [coins, setCoins] = useState(0);
-  const signedInUser = JSON.parse(sessionStorage.user);
+  const [userId, setUserId] = useState(user._id);
+  // const signedInUser = JSON.parse(sessionStorage.user);
 
-  const getSignedInUserData = (userId) => {
-    axios
-      .get(`/user/${userId}`)
-      .then(({ data }) => {
-        setUser(data[0]);
-        setCoins(data[0].coinCount);
-      })
-      .catch((err) => console.error('get signed in user ERROR', err));
-  };
+  // const getSignedInUserData = (userId) => {
+  //   axios
+  //     .get(`/user/${userId}`)
+  //     .then(({ data }) => {
+  //       setUser(data[0]);
+  //       setCoins(data[0].coinCount);
+  //     })
+  //     .catch((err) => console.error('get signed in user ERROR', err));
+  // };
+  // useEffect(() => {
+  //   getSignedInUserData(signedInUser._id);
+  //   //use storage to get user from db the set user state as db user obj
+  // }, []);
   useEffect(() => {
-    getSignedInUserData(signedInUser._id);
-    //use storage to get user from db the set user state as db user obj
+    setUserId(user._id);
+    getDogs();
+    axios.get(`/user/${user._id}`).then((userData) => {
+      setCoins(userData.data[0].coinCount);
+    });
   }, []);
+
+  const handleSubscribe = () => {
+    if (dogView === '' || dogName === '') {
+      alert('Fill all fields');
+    } else if (coins >= 200) {
+      axios
+        .post('/dog', {
+          name: dogName,
+          img: dogView,
+          owner: userId,
+          groom: true,
+        })
+        .then(({ data }) => {
+          setCoins(data.coinCount);
+        });
+      getDogs();
+      setDogs([]);
+      setList([]);
+    } else {
+      alert('Not enough coins!');
+    }
+    setShop(false);
+  };
 
   return (
     <Container>
@@ -62,6 +93,26 @@ function Groom({ dogs }) {
         </Col>
         <Col xs={1}></Col>
       </Row>
+      <div>
+        <Form.Group>
+          <Form.Label>15 coins:</Form.Label>
+          <Button
+            variant='primary'
+            type='submit'
+            onClick={() => handleSubmit()}
+          >
+            Buy Dog
+          </Button>
+          <Form.Label>200 coins:</Form.Label>
+          <Button
+            variant='primary'
+            type='submit'
+            onClick={() => handleSubscribe()}
+          >
+            Subscribe to Groomer!
+          </Button>
+        </Form.Group>
+      </div>
     </Container>
   );
 }
