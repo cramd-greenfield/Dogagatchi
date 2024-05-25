0f618759eec1daf4eed01219e17ee5b4eecb6fda
cody-daigle <cody.h.daigle@gmail.com>
2024-05-24 23:15:53 -0500

    Created custom card for 'Groomed' dogs

--- client/components/Groom.jsx
+++ client/components/Groom.jsx
@@ -1,35 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {
-  Button,
-  ProgressBar,
-  Card,
-  Dropdown,
-  DropdownButton,
-  Modal,
-} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, ProgressBar, Card, Form } from 'react-bootstrap';
 import axios from 'axios';
 import barkSound from '../../server/barking-123909.mp3';
 
-const Groom = (props) => {
-  const { dogObj, setCoins, coins } = props;
-  const [dog, setDog] = useState(dogObj);
-  const [hungry, setHunger] = useState(true);
-  const [happy, setHappy] = useState(false);
+const Groom = () => {
+  const [dog, setDog] = useState([]);
+  const [hungry, setHunger] = useState(false);
+  const [happy, setHappy] = useState(true);
+  const [groomed, setGroomed] = useState([]);
   const [feedStatus, setFeedStatus] = useState('');
   const [walkStatus, setWalkStatus] = useState('');
   const [feedTimer, setFeedTimer] = useState(0);
   const [walkTimer, setWalkTimer] = useState(0);
-  const [groom, setGroom] = useState([]);
+  const [groom, setGroom] = useState(false);
 
   const user = JSON.parse(sessionStorage.getItem('user'));
 
   const hungryRef = useRef(null);
   const happyRef = useRef(null);
-
-  useEffect(() => {
-    getGroomedDogs(user._id);
-  }, []);
 
   const getDog = () => {
     axios
@@ -40,151 +28,96 @@ const Groom = (props) => {
       });
   };
 
-  const getGroomedDogs = () => {
+  const upgradeDog = () => {
     axios
-      .get(`/groom/groomed/${user._id}`)
-      .then(({ data }) => {
-        console.log(data);
-      })
-      .catch((err) => console.error('get signed in user ERROR', err));
-  };
-
-  const feedDog = (dogToFeedObj, mealToFeedObj) => {
-    const status = {
-      feedDeadline: new Date(
-        new Date(dogToFeedObj.feedDeadline).getTime() + 24 * 60 * 60 * 1000
-      ),
-      walkDeadline: new Date(
-        new Date(dogToFeedObj.walkDeadline).getTime() + 12 * 60 * 60 * 1000
-      ),
-    };
-
-    axios
-      .put(`dog/${dogToFeedObj._id}`, { status })
+      .put(`dog/${dog._id}`, { status })
       .then(getDog())
       .then(() => {
-        axios
-          .put(`user/meals/${user._id}`, {
-            update: {
-              type: 'deleteMeal',
-            },
-            mealToDelete: mealToFeedObj,
-          })
-          .then(() => getSignedInUserMeals(user._id));
+        axios.put(`user/meals/${user._id}`, {}).then(() => getGroomedDogs());
       })
       .catch((err) => console.error('feed dog meal ERROR:', err));
   };
 
-  const handleClick = (e) => {
-    const status = {};
-
-    if (e === 'walk') {
-      setHappy(true);
-      happyRef.current = happy;
-      const walkDeadline = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-      status.walkDeadline = walkDeadline;
-      setWalkTimer(walkDeadline);
+  const subscribe = () => {
+    if (coins >= 200) {
       axios
-        .put(`/dog/${dog._id}`, { status })
-        .then(() => getDog())
-        .catch((err) => {
-          console.error(err);
-        });
-    } else if (e === 'feed' && coins < 3) {
-      alert('Not enough coins!');
-    } else if (e === 'feed' && coins >= 3) {
-      setHunger(false);
-      hungryRef.current = hungry;
-      const feedDeadline = Date.parse(dog.feedDeadline) + 12 * 60 * 60 * 1000;
-      status.feedDeadline = feedDeadline;
-      setFeedTimer(feedDeadline);
-      axios
-        .put(`/dog/${dog._id}`, { status, cost: -3 })
+        .patch(`/groom/${dog._id}`, {
+          isGroomed: groomed,
+        })
         .then(({ data }) => {
           setCoins(data.coinCount);
-        })
-        .then(() => getDog())
-        .catch((err) => {
-          console.error(err);
         });
+      getDog();
+      setDog([]);
+      setGroomed(true);
     } else {
-      bark.play();
+      alert('Not enough coins!');
     }
+    setShop(false);
   };
-
-  const fetchAndShowWord = () => {
-    // request to /words/:dogId
-    axios
-      .post(`/words/${dog._id}`)
-      .then(({ data }) => {
-        console.log('data recd from axios post');
-        console.log('keys', Object.keys(data));
-        console.log('data', data.meanings);
-        setWord(data);
-        setShowWord(true);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-  const handleCloseWord = () => setShowWord(false);
 
   useEffect(() => {
     getDog();
   }, [happy, hungry]);
 
   useEffect(() => {
-    const now = new Date().getTime();
+    const x = setInterval(() => {
+      const now = new Date().getTime();
 
-    const feedTimer = ((Date.parse(dog.feedDeadline) - now) / 86400000) * 100;
-    const walkTimer = ((Date.parse(dog.walkDeadline) - now) / 86400000) * 100;
+      const feedTimer = ((Date.parse(dog.feedDeadline) - now) / 86400000) * 100;
+      const walkTimer = ((Date.parse(dog.walkDeadline) - now) / 86400000) * 100;
 
-    setFeedTimer(feedTimer);
-    setWalkTimer(walkTimer);
+      setFeedTimer(feedTimer);
+      setWalkTimer(walkTimer);
 
-    if (feedTimer < 25) {
-      setFeedStatus('danger');
-      if (hungryRef.current !== true) {
-        setHunger(true);
-        hungryRef.current = hungry;
+      if (feedTimer < 25) {
+        setFeedStatus('danger');
+        if (hungryRef.current !== true) {
+          setHunger(true);
+          hungryRef.current = hungry;
+        }
+      } else if (feedTimer < 50) {
+        setFeedStatus('warning');
+        if (hungryRef.current !== true) {
+          setHunger(true);
+          hungryRef.current = hungry;
+        }
+      } else {
+        setFeedStatus('success');
+        if (hungryRef.current !== false) {
+          setHunger(false);
+          hungryRef.current = hungry;
+        }
       }
-    } else if (feedTimer < 50) {
-      setFeedStatus('warning');
-      if (hungryRef.current !== true) {
-        setHunger(true);
-        hungryRef.current = hungry;
-      }
-    } else {
-      setFeedStatus('success');
-      if (hungryRef.current !== false) {
-        setHunger(false);
-        hungryRef.current = hungry;
-      }
-    }
 
-    if (walkTimer < 25) {
-      setWalkStatus('danger');
-      if (happyRef.current !== false) {
-        setHappy(false);
-        happyRef.current = happy;
+      if (walkTimer < 25) {
+        setWalkStatus('danger');
+        if (happyRef.current !== false) {
+          setHappy(false);
+          happyRef.current = happy;
+        }
+      } else if (walkTimer < 50) {
+        setWalkStatus('warning');
+        if (happyRef.current !== false) {
+          setHappy(false);
+          happyRef.current = happy;
+        }
+      } else {
+        setWalkStatus('success');
+        if (happyRef.current !== true) {
+          setHappy(true);
+          happyRef.current = happy;
+        }
       }
-    } else if (walkTimer < 50) {
-      setWalkStatus('warning');
-      if (happyRef.current !== false) {
-        setHappy(false);
-        happyRef.current = happy;
-      }
-    } else {
-      setWalkStatus('success');
-      if (happyRef.current !== true) {
-        setHappy(true);
-        happyRef.current = happy;
-      }
-    }
+    }, 1000);
+    return () => clearInterval(x);
   }, [happy, hungry, dog]);
 
   return (
-    <Card className='d-flex flex-row m-4'>
+    <Card
+      variant='warning'
+      className='d-flex flex-row m-4 card text-white bg-warning mb-3'
+    >
       <div
         className='d-flex flex-column justify-content-center align-items-center align-self-center'
         style={{ width: '250px', height: '250px' }}
@@ -194,6 +127,11 @@ const Groom = (props) => {
           alt='Sorry, your dog is in another kennel.'
           className='p-4'
         />
+
+        <Button variant='warning' onClick={() => subscribe}>
+          💎 Groom 💎
+        </Button>
+        <Form.Label>200 Coins!</Form.Label>
       </div>
       <div className='d-flex flex-column justify-content-center align-items-center w-100'>
         <Card.Title className='pt-2'>{dog.name}</Card.Title>
@@ -203,28 +141,54 @@ const Groom = (props) => {
               animated={true}
               striped
               variant='warning'
+              now={feedTimer}
               label='FAVORITE'
               style={{ height: '35px' }}
             />
-
-            <Button
-              className='w-100 mx-0'
-              variant='info'
-              onClick={() => handleClick('bark')}
-            >
-              🦴
-            </Button>
+            {hungry ? (
+              <Button
+                className='w-100 mx-0'
+                variant='info'
+                onClick={() => handleClick('feed')}
+              >
+                🍖
+              </Button>
+            ) : (
+              <Button
+                className='w-100 mx-0'
+                variant='info'
+                onClick={() => handleClick('bark')}
+              >
+                🦴
+              </Button>
+            )}
             <ProgressBar
               animated={true}
               striped
               variant='warning'
+              now={walkTimer}
               label='FAVORITE'
               style={{ height: '35px' }}
             />
-            {/* Will be back for this please don't remove - Cody */}
-            {/* <Button type='submit' variant='warning' onClick={subscribe}>
-                💎Groom Subscription💎
-              </Button> */}
+
+            {groom ? (
+              <DropdownButton title='Feed from Pantry!'>
+                {groom.map((dog) => (
+                  <Dropdown.Item
+                    key={dog._id}
+                    onClick={(e) => {
+                      setGroom(e.target.value);
+                    }}
+                  >
+                    {meal.name}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+            ) : (
+              <Form.Label>
+                Visit Bone Appétit Café to buy your first meal!
+              </Form.Label>
+            )}
           </div>
         </Card.Body>
       </div>
--- client/components/Grooms.jsx
+++ client/components/Grooms.jsx
@@ -2,58 +2,136 @@ import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Groom from './Groom.jsx';
 
-const Grooms = () => {
-  const [dogShop, setShop] = useState(false);
-  const [breeds, setBreeds] = useState([]);
-  const [dogView, setDogView] = useState('');
-  const [dogName, setDogName] = useState('');
+const Grooms = ({ dogs, setCoins, coins }) => {
   const user = JSON.parse(sessionStorage.getItem('user'));
-  const [userId, setUserId] = useState(user._id);
 
-  const getDogs = () => {
-    axios
-      .get(`/groom/${userId}`)
-      .then(({ data }) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
-  const handleSelect = (img) => {
-    console.log('hit', img);
-    setDogView(img);
-  };
-
-  const subscribe = () => {
-    if (dogView === '' || dogName === '') {
-      alert('Fill all fields');
-    } else if (coins >= 15) {
-      axios
-        .post(`/groom/${dog._id}`, {
-          name: dogName,
-          img: dogView,
-          owner: userId,
-        })
-        .then(({ data }) => {
-          setCoins(data.coinCount);
-        });
-      getDogs();
-      setDogs([]);
-      setList([]);
-    } else {
-      alert('Not enough coins!');
-    }
-    setShop(false);
-  };
   return (
-    <div>
-      {/* {dogs.map((dog) => (
-        <Groom dog={dog} key={dog._id} />
-       ))} */}
+    <div
+      style={{
+        height: 800,
+        overflow: 'auto-hidden',
+      }}
+    >
+      {Array.isArray(dogs) && dogs.length > 0
+        ? dogs
+            .filter((dog) => {
+              if (dog.isGroomed === true) {
+                axios
+                  .get(`/groom/member/${user._id}`)
+                  .then(() => alert(`${dog.name} now has a personal groomer!`))
+                  .catch((err) => {
+                    console.error(err);
+                  });
+                return false;
+              } else {
+                return true;
+              }
+            })
+            .map((dog) => {
+              return (
+                <div className='item' key={dog._id}>
+                  <Groom
+                    coins={coins}
+                    setCoins={setCoins}
+                    dogObj={dog}
+                    dogs={dogs}
+                    // setDogs={setDogs}
+                  />
+                </div>
+              );
+            })
+        : ''}
     </div>
   );
 };
-
 export default Grooms;
+
+// const [dogShop, setShop] = useState(false);
+// const [breeds, setBreeds] = useState([]);
+// const [groom, setGroom] = useState([]);
+// const [dogView, setDogView] = useState('');
+// const [dogName, setDogName] = useState('');
+// const user = JSON.parse(sessionStorage.getItem('user'));
+// const [userId, setUserId] = useState(user._id);
+
+// const getDogs = () => {
+//   axios
+//     .get(`/groomed/${user._d}`)
+//     .then(({ data }) => {
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// };
+
+// const handleSelect = (img) => {
+//   console.log('hit', img);
+//   setDogView(img);
+// };
+
+// const subscribe = () => {
+//   if (coins >= 200) {
+//     axios
+//       .post(`/groom/${dog._id}`, {
+//         name: dogName,
+//         img: dogView,
+//         owner: userId,
+//         isGroomed: groom,
+//       })
+//       .then(({ data }) => {
+//         setCoins(data.coinCount);
+//       });
+//     getDogs();
+//     setDogs([]);
+//     setGroom([]);
+//   } else {
+//     alert('Not enough coins!');
+//   }
+//   setShop(false);
+// };
+// return (
+//   <div>
+//     <Groom getDogs={getDogs} />
+//     <Image src={dogView} alt='' rounded style={{ width: 200 }} />
+//     <Form>
+//       <Form.Group>
+//         <Form.Label>Name</Form.Label>
+//         <Form.Control
+//           placeholder='Dog name'
+//           onChange={(e) => setDogName(e.target.value)}
+//         />
+//       </Form.Group>
+//       <Form.Group>
+//         {/*  <Dropdown>
+//               <Dropdown.Toggle
+//                 style={{ width: '300px' }}
+//                 onSelect={() => {
+//                   handleSelect(dog);
+//                 }}
+//                 variant='success'
+//                 id='dropdown-basic'
+//               >
+//                 Select Dog
+//               </Dropdown.Toggle>
+//               <Dropdown.Menu
+//                 style={{ maxHeight: '300px', overflowY: 'auto' }}
+//               >
+//                 {breeds.map((dog, index) => (
+//                   <Dropdown.Item
+//                     onClick={() => setDogView(dog)}
+//                     eventKey={dog}
+//                     key={index}
+//                   >
+//                     <img src={dog} style={{ width: '250px' }} />
+//                   </Dropdown.Item>
+//                 ))}
+//               </Dropdown.Menu>
+//             </Dropdown> */}
+//       </Form.Group>
+//     </Form>
+//   </div>
+// );
+// };
+
+// export default Grooms;
