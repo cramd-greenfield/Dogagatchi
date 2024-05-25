f5501faf27994497593c52f3b131f42d97f01035
cody-daigle <cody.h.daigle@gmail.com>
2024-05-24 22:24:37 -0500

    Removed isGroomed item from req.params for post

--- server/routes/dogRoutes.js
+++ server/routes/dogRoutes.js
@@ -49,7 +49,7 @@ router.get('/id/:dogId', (req, res) => {
 //POST DOG
 
 router.post('/', (req, res) => {
-  const { name, img, owner, isGroomed } = req.body;
+  const { name, img, owner } = req.body;
   const status = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 
   Dog.create({
