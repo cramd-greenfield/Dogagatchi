a45dd9e196c84c022febea110645dbf6ddff41cc
cody-daigle <cody.h.daigle@gmail.com>
2024-05-25 18:23:16 -0500

    Added Groomed Dog card when clicking subscribe

--- client/components/Dog.jsx
+++ client/components/Dog.jsx
@@ -11,7 +11,6 @@ import {
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import barkSound from '../../server/barking-123909.mp3';
-import Grooms from './Grooms.jsx';
 
 const bark = new Audio(barkSound);
 
@@ -27,7 +26,8 @@ function Dog(props) {
   const [meals, setMeals] = useState([]);
   const [word, setWord] = useState({});
   const [showWord, setShowWord] = useState(false);
-  const [groomed, setGroomed] = useState([]);
+  const [groom, setGroom] = useState([]);
+  const [groomed, setGroomed] = useState(false);
   const user = JSON.parse(sessionStorage.getItem('user'));
 
   const hungryRef = useRef(null);
@@ -92,8 +92,8 @@ function Dog(props) {
         setCoins(data.coinCount);
       });
       getDog();
-      setDog([]);
-      setGroomed([]);
+      setGroom([]);
+      setGroomed(true);
     } else {
       alert('Not enough coins!');
     }
@@ -213,154 +213,196 @@ function Dog(props) {
   }, [happy, hungry, dog]);
 
   return (
-    <Card className='d-flex flex-row m-4'>
-      <div
-        className='d-flex flex-column justify-content-center align-items-center align-self-center '
-        style={{ width: '250px', height: '250px' }}
-      >
-        <Card.Img
-          src={dog.img}
-          alt='Sorry, your dog is in another kennel.'
-          className='p-4'
-        />
-
-        <Button variant='warning' onClick={subscribe}>
-          💎 Groom 💎
-        </Button>
-        <Form.Label>200 Coins!</Form.Label>
-      </div>
-      <div className='d-flex flex-column justify-content-center align-items-center w-100 '>
-        <Card.Title className='pt-2'>{dog.name}</Card.Title>
-        <Card.Body className='w-100'>
-          <div className='dog-status'>
-            <ProgressBar
-              animated={true}
-              striped
-              variant={feedStatus}
-              now={feedTimer}
-              label='HUNGER'
-              style={{ height: '35px' }}
+    <div>
+      {groomed ? (
+        <Card className='d-flex flex-row m-4 card text-white bg-warning mb-3'>
+          <div
+            className='d-flex flex-column justify-content-center align-items-center align-self-center'
+            style={{ width: '250px', height: '250px' }}
+          >
+            <Card.Img
+              src={dog.img}
+              alt='Sorry, your dog does not want to be seen with you...'
+              className='p-4'
             />
-            {hungry ? (
-              <Button
-                className='w-100 mx-0'
-                variant='info'
-                onClick={() => handleClick('feed')}
-                title={'pay 3 coins'}
-              >
-                🍖
-              </Button>
-            ) : (
-              <Button
-                className='w-100 mx-0'
-                variant='info'
-                onClick={() => handleClick('bark')}
-              >
-                🦴
-              </Button>
-            )}
-            <ProgressBar
-              animated={true}
-              striped
-              variant={walkStatus}
-              now={walkTimer}
-              label='HAPPINESS'
-              style={{ height: '35px' }}
-            />
-
-            {happy ? (
-              <Button
-                className='w-100 mx-0'
-                variant='info'
-                onClick={() => handleClick('bark')}
-              >
-                🐶
-              </Button>
-            ) : (
-              <Button
-                className='w-100 mx-0'
-                variant='info'
-                onClick={() => handleClick('walk')}
-              >
-                🐕‍🦺
-              </Button>
-            )}
-            {meals ? (
-              <DropdownButton title='Feed from Pantry!'>
-                {meals.map((meal) => (
-                  <Dropdown.Item
-                    key={meal._id}
-                    onClick={() => {
-                      feedDog(dog, meal);
-                    }}
-                  >
-                    {meal.name}
-                  </Dropdown.Item>
-                ))}
-              </DropdownButton>
-            ) : (
-              <DropdownButton title='Feed from Pantry!'>
-                <Dropdown.Item>
-                  Visit Bone Appétit Café to buy your first meal!
-                </Dropdown.Item>
-              </DropdownButton>
-            )}
-
-            <Button onClick={fetchAndShowWord}>Word of the Day!</Button>
-            <Modal show={showWord} onHide={handleCloseWord}>
-              <Modal.Header closeButton>
-                <Modal.Title>Word Of The Day</Modal.Title>
-              </Modal.Header>
-
-              {showWord ? (
-                <Modal.Body>
-                  <h2>{word.word}</h2>
-                  <p>{word.phonetic}</p>
-                  {word.meanings.map((meaning) => {
-                    return (
-                      <>
-                        <em>{meaning.partOfSpeech}</em>
-                        {meaning.definitions.map((def, i) => {
-                          return <p>{`${i + 1}: ${def}`}</p>;
-                        })}
-                      </>
-                    );
-                  })}
-                </Modal.Body>
-              ) : (
-                <h2>placeholder</h2>
-              )}
-
-              {showWord ? (
-                <Modal.Body>
-                  <h2>{word.word}</h2>
-                  <p>{word.phonetic}</p>
-                  {word.meanings.map((meaning, i) => {
-                    return (
-                      <div key={i}>
-                        <em>{meaning.partOfSpeech}</em>
-                        {meaning.definitions.map((def, i) => {
-                          return <p key={i}>{`${i + 1}: ${def}`}</p>;
-                        })}
-                      </div>
-                    );
-                  })}
-                </Modal.Body>
-              ) : (
-                <h2>placeholder</h2>
-              )}
-
-              <Modal.Footer>
-                <Button variant='secondary' onClick={handleCloseWord}>
-                  Close
-                </Button>
-                <Button variant='primary'>Add to Dogtionary!</Button>
-              </Modal.Footer>
-            </Modal>
           </div>
-        </Card.Body>
-      </div>
-    </Card>
+          <div className='d-flex flex-column justify-content-center align-items-center w-100'>
+            <Card.Title className='pt-2'>{dog.name}</Card.Title>
+            <Card.Body className='w-100'>
+              <div className='dog-status'>
+                <ProgressBar
+                  animated={true}
+                  striped
+                  now={100}
+                  variant='warning'
+                  label='FAVORITE'
+                  style={{ height: '35px' }}
+                />
+                <Button onClick={fetchAndShowWord}>Word of the Day!</Button>
+                <Modal show={showWord} onHide={handleCloseWord}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Word Of The Day</Modal.Title>
+                  </Modal.Header>
+                  {showWord ? (
+                    <Modal.Body>
+                      <h2>{word.word}</h2>
+                      <p>{word.phonetic}</p>
+                      {word.meanings.map((meaning) => {
+                        return (
+                          <>
+                            <em>{meaning.partOfSpeech}</em>
+                            {meaning.definitions.map((def, i) => {
+                              return <p>{`${i + 1}: ${def}`}</p>;
+                            })}
+                          </>
+                        );
+                      })}
+                    </Modal.Body>
+                  ) : (
+                    <h2>placeholder</h2>
+                  )}
+                  <Modal.Footer>
+                    <Button variant='secondary' onClick={handleCloseWord}>
+                      Close
+                    </Button>
+                    <Button variant='primary'>Add to Dogtionary!</Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
+            </Card.Body>
+          </div>
+        </Card>
+      ) : (
+        <Card className='d-flex flex-row m-4'>
+          <div
+            className='d-flex flex-column justify-content-center align-items-center align-self-center '
+            style={{ width: '250px', height: '250px' }}
+          >
+            <Card.Img
+              src={dog.img}
+              alt='Sorry, your dog is in another kennel.'
+              className='p-4'
+            />
+
+            <Button variant='warning' onClick={subscribe}>
+              💎 Groom 💎
+            </Button>
+            <Form.Label>200 Coins!</Form.Label>
+          </div>
+          <div className='d-flex flex-column justify-content-center align-items-center w-100 '>
+            <Card.Title className='pt-2'>{dog.name}</Card.Title>
+            <Card.Body className='w-100'>
+              <div className='dog-status'>
+                <ProgressBar
+                  animated={true}
+                  striped
+                  variant={feedStatus}
+                  now={feedTimer}
+                  label='HUNGER'
+                  style={{ height: '35px' }}
+                />
+                {hungry ? (
+                  <Button
+                    className='w-100 mx-0'
+                    variant='info'
+                    onClick={() => handleClick('feed')}
+                    title={'pay 3 coins'}
+                  >
+                    🍖
+                  </Button>
+                ) : (
+                  <Button
+                    className='w-100 mx-0'
+                    variant='info'
+                    onClick={() => handleClick('bark')}
+                  >
+                    🦴
+                  </Button>
+                )}
+                <ProgressBar
+                  animated={true}
+                  striped
+                  variant={walkStatus}
+                  now={walkTimer}
+                  label='HAPPINESS'
+                  style={{ height: '35px' }}
+                />
+
+                {happy ? (
+                  <Button
+                    className='w-100 mx-0'
+                    variant='info'
+                    onClick={() => handleClick('bark')}
+                  >
+                    🐶
+                  </Button>
+                ) : (
+                  <Button
+                    className='w-100 mx-0'
+                    variant='info'
+                    onClick={() => handleClick('walk')}
+                  >
+                    🐕‍🦺
+                  </Button>
+                )}
+                {meals ? (
+                  <DropdownButton title='Feed from Pantry!'>
+                    {meals.map((meal) => (
+                      <Dropdown.Item
+                        key={meal._id}
+                        onClick={() => {
+                          feedDog(dog, meal);
+                        }}
+                      >
+                        {meal.name}
+                      </Dropdown.Item>
+                    ))}
+                  </DropdownButton>
+                ) : (
+                  <DropdownButton title='Feed from Pantry!'>
+                    <Dropdown.Item>
+                      Visit Bone Appétit Café to buy your first meal!
+                    </Dropdown.Item>
+                  </DropdownButton>
+                )}
+
+                <Button onClick={fetchAndShowWord}>Word of the Day!</Button>
+                <Modal show={showWord} onHide={handleCloseWord}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Word Of The Day</Modal.Title>
+                  </Modal.Header>
+                  {showWord ? (
+                    <Modal.Body>
+                      <h2>{word.word}</h2>
+                      <p>{word.phonetic}</p>
+                      {word.meanings.map((meaning) => {
+                        return (
+                          <>
+                            <em>{meaning.partOfSpeech}</em>
+                            {meaning.definitions.map((def, i) => {
+                              return <p>{`${i + 1}: ${def}`}</p>;
+                            })}
+                          </>
+                        );
+                      })}
+                    </Modal.Body>
+                  ) : (
+                    <h2>placeholder</h2>
+                  )}
+
+                  <Modal.Footer>
+                    <Button variant='secondary' onClick={handleCloseWord}>
+                      Close
+                    </Button>
+                    <Button variant='primary'>Add to Dogtionary!</Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
+            </Card.Body>
+          </div>
+        </Card>
+      )}
+    </div>
   );
 }
 
